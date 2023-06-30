f8c8eb0205ee603c17590a2b631c4f89ae9d308e
wilson melo <bakudas@gmail.com>
2023-06-29 23:03:28 -0300

    LRtHW book ex6

---
+++ livros/learnrubythehardway/ex6/ex6.rb
@@ -0,0 +1,21 @@
+tipos_de_pessoas = 10
+x = "Existem #{tipos_de_pessoas} tipos de pessoas"
+binario = 'binário'
+nao = 'não'
+y = "Alguns sabem o que é #{binario}, outros #{nao}"
+
+puts x
+puts y
+
+puts "Eu disse: #{x}"
+puts "Eu também disse: #{y}"
+
+hilario = false
+avaliacao_piada = "Essa piada foi engraçada? #{hilario}"
+
+puts avaliacao_piada
+
+w = 'Isso é o lado esquerdo da..'
+e = 'string com um lado direito.'
+
+puts w + e
