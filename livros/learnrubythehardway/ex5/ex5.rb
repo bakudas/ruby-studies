0410e692d2a314a648b0b1e11c3a8df8b5c6191e
wilson melo <bakudas@gmail.com>
2023-06-29 22:52:50 -0300

    LRtHW book ex5

---
+++ livros/learnrubythehardway/ex5/ex5.rb
@@ -0,0 +1,13 @@
+nome = 'bakudas'
+idade = 34
+altura = 170
+peso = 99
+cor_olho = 'castanho'
+cor_cabelo = 'castanho'
+
+puts "O meu nome é #{nome}"
+puts "Tenho #{altura} de altura"
+puts "Meu peso atual é #{peso}"
+puts "A cor dos meu olho é #{cor_olho} e a do meu cabelo é #{cor_cabelo}"
+
+puts "E eu somar #{idade}, #{altura} e #{peso} eu obtenho #{idade + altura + peso}"
