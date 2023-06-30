a051448beb47ce4db6b67b128dc0b23f2ada3172
wilson melo <bakudas@gmail.com>
2023-06-30 05:59:26 -0300

    LRtHW book ex9

---
+++ livros/learnrubythehardway/ex9/ex9.rb
@@ -0,0 +1,14 @@
+# Exercise 9: Printing, Printing, Printing
+
+dias = "Seg Ter Qua Qui Sex Sab Dom"
+meses = "Jan\nFev\nMar\nAbr\nMai\nJun\nJul\nAgo\nSet\nOut\nNov\nDez"
+
+puts "Aqui estão os dias: #{dias}"
+puts "Aqui estão os meses: #{meses}"
+
+puts %q{
+Tem alguma coisa acontecendo aqui..
+com esse quote estranho
+Somos capazes de escrever da forma como quisermos.
+Até mesmo 4 linhas dessa maneiras, ou 5, ou 6..
+}
