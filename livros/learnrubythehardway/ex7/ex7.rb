3a422a27f2a5cd0c1f523276eb4b8813a07cc5a5
wilson melo <bakudas@gmail.com>
2023-06-29 23:22:17 -0300

    LRtHW book ex7

---
+++ livros/learnrubythehardway/ex7/ex7.rb
@@ -0,0 +1,24 @@
+puts 'Mary tem um pequeno cordeiro.'
+puts 'A lã dele é branca como a neve.'
+puts 'E todo lugar que Mary vai.'
+puts '.' * 10
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# print: printa e não pula linha no final
+# puts: printa e pula linha
+# p: printa a string raw e pula linha
+p end1 + end2 + end3 + end4 + end5 + end6
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
