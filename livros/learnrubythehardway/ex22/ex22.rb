3976534bfb3997326c7714cc2961fd4cb46e6404
wilson melo <bakudas@gmail.com>
2023-07-03 18:10:57 -0300

    LRtHW book ex22, 23 and 24

---
+++ livros/learnrubythehardway/ex22/ex22.rb
@@ -0,0 +1,3 @@
+# Exercise 22: What dou you know so far?
+
+# just a review
---
+++ livros/learnrubythehardway/ex24/ex24.rb
@@ -0,0 +1,35 @@
+# Exercise 24: Mora Practice
+
+puts "Let's practice everything."
+puts "You\'d need to know \'bout escapes with \\ that \n do newlines and \t tabs."
+
+# the << is "heredoc". END is the token to finalize heredoc
+poem = <<END
+ruby
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\twhere there is none.
+END
+
+puts "--------------"
+puts poem
+puts "--------------"
+
+five = 10 - 2 + 3 - 6
+puts "This should be five: #{five}"
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+puts "With a starting pointing of: #{start_point}"
+puts "We'd have %s beans, %d jars and %d crates." % secret_formula(start_point)
